4167325f851753df72fc91b2b62ad53ee6f9bcaf
azat44 <azat4beats@gmail.com>
2024-12-27 16:35:56 +0100

    added main page landscape for mobile

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Donation from "./Components/Donation";
 
 const App = () => {
+  
+  useEffect(() => {
+    const handleOrientation = () => {
+      if (window.innerHeight < window.innerWidth) {
+        // Si paysage, forcer un retour en portrait
+        document.body.style.transform = "rotate(0deg)";
+        document.body.style.overflow = "hidden";
+      } else {
+        document.body.style.transform = "none";
+        document.body.style.overflow = "auto";
+      }
+    };
+  
+    window.addEventListener("resize", handleOrientation);
+    handleOrientation();
+  
+    return () => {
+      window.removeEventListener("resize", handleOrientation);
+    };
+  }, []);
+  
+
   return (
     <Router>
       <Routes>
